3925c64b57e915d170d2a003e4072d0df21b967e
taewan <97833069+qqq3964@users.noreply.github.com>
2023-04-06 21:15:00 +0900

    2023/4/6

--- 0x00/2504.py
+++ 0x00/2504.py
@@ -1,1 +1,59 @@
-print(1)
+def solution(arr):
+    stack = []
+    check = []
+    for idx,val in enumerate(arr):
+        if len(stack) == 0:
+            stack.append((val,idx))
+        else:
+            pre_val,pre_idx = stack[-1]
+            if pre_val == '(' and val == ')':
+                if check:
+                    temp = []
+                    rm_temp = []
+                    for i in check:
+                        check_val,check_idx = i[0],i[1]
+                        if check_idx < idx and check_idx > pre_idx:
+                            temp.append(check_val)
+                            rm_temp.append(i)
+                    for i in rm_temp:
+                        check.remove(i)
+                    sum_result = sum(temp)*2
+
+                    if sum_result != 0:
+                        check.append((sum_result,idx))
+                    else:
+                        check.append((2,idx))
+                else:
+                    check.append((2,idx))
+                stack.pop()
+            elif pre_val == '[' and val == ']':
+                if check:
+                    temp = []
+                    rm_temp = []
+                    for i in check:
+                        check_val,check_idx = i[0],i[1]
+                        if check_idx < idx and check_idx > pre_idx:
+                            temp.append(check_val)
+                            rm_temp.append(i)
+                    for i in rm_temp:
+                        check.remove(i)
+                    sum_result = sum(temp)*3
+                    if sum_result != 0:
+                        check.append((sum_result,idx))
+                    else:
+                        check.append((3,idx))
+                else:
+                    check.append((3,idx))
+                stack.pop()
+            else:
+                stack.append((val,idx))
+    result = 0
+    for i in check:
+        result+=i[0]
+    if stack:
+        print(0)
+    else:
+        print(result)
+if __name__ == '__main__':
+    arr = input()
+    solution(arr)

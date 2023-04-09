b7d140ef9949dc0c28a0b85b8c722a7f52e08ab2
qqq3964 <qqq3964@naver.com>
2023-04-09 10:40:16 +0900

    2023/4/9

--- 0x01/6593.py
+++ 0x01/6593.py
@@ -1,8 +1,8 @@
 from collections import deque
 
-dx = [1,0,-1,0]
-dy = [0,1,0,-1]
-
+dx = [1,0,-1,0,0,0]
+dy = [0,1,0,-1,0,0]
+dz = [0,0,0,0,1,-1]
 
 def solution(L,R,C,board):
     # S,E찾기
@@ -26,26 +26,16 @@ def solution(L,R,C,board):
     visited[S[0]][S[1]][S[2]] = 1
     while queue:
         height,x,y = queue.popleft()
-        for idx in range(4):
+        for idx in range(6):
             rx = dx[idx]+x
             ry = dy[idx]+y
-            north_h = height+1
-            south_h = height-1
-            if rx<0 or ry<0 or rx>=R or ry>=C:
+            rz = dz[idx]+height
+            if rx<0 or ry<0 or rx>=R or ry>=C or rz<0 or rz>=L:
                 continue
+            if (board[rz][rx][ry] == '.' or board[rz][rx][ry] == 'E') and visited[rz][rx][ry] == 0:
+                queue.append((rz,rx,ry))
+                visited[rz][rx][ry] = visited[height][x][y] + 1
 
-            if (board[height][rx][ry] == '.' or board[height][rx][ry] == 'E') and visited[height][rx][ry] == 0:
-                queue.append((height,rx,ry))
-                visited[height][rx][ry] = visited[height][x][y] + 1
-
-            if north_h < L and (board[north_h][x][y] == '.' or board[north_h][x][y] == 'E') and visited[north_h][x][y] == 0:
-                queue.append((north_h,x,y))
-                visited[north_h][x][y] = visited[height][x][y] + 1
-
-            if south_h >= 0 and (board[south_h][x][y] == '.' or board[south_h][x][y] == 'E') and visited[south_h][x][y] == 0:
-                queue.append((south_h,x,y))
-                visited[south_h][x][y] = visited[height][x][y] + 1
-            
     result = visited[E[0]][E[1]][E[2]] - 1 
     if result == -1:
         print("Trapped!")
